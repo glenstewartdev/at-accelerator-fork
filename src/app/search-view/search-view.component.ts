41cc4a9ba96656190e1140f06fbea1f4bc7f8aef
glenstewartdev <glenstewartdev@gmail.com>
2024-08-22 18:49:29 -0400

    remove uneeded import

--- src/app/search-view/search-view.component.ts
+++ src/app/search-view/search-view.component.ts
@@ -1,12 +1,11 @@
 import { Component, EventEmitter, OnInit, Output, computed, inject, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {TvShowTableComponent} from '../tv-show-table/tv-show-table.component';
 import { TvShowsService } from './../services/tv-shows/tv-shows.service';
 
 @Component({
   selector: 'app-search-view',
   standalone: true,
-  imports: [CommonModule, TvShowTableComponent],
+  imports: [CommonModule],
   templateUrl: './search-view.component.html',
   styleUrls: ['./search-view.component.css']
 })
