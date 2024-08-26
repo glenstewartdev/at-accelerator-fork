c8a7d092dd20cc4eef9381de82fe1930847c1fb0
glenstewartdev <glenstewartdev@gmail.com>
2024-08-26 11:13:57 -0400

    add local-storage service

---
+++ src/app/services/local-storage/local-storage.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  constructor() { }
+
+  setItem<T>(key: string, item: T): void {
+    const jsonString = JSON.stringify(item);
+    localStorage.setItem(key, jsonString);
+  }
+
+  getItem<T>(key: string): T | null {
+    const jsonString = localStorage.getItem(key);
+    return jsonString ? JSON.parse(jsonString) as T : null;
+  }
+}
