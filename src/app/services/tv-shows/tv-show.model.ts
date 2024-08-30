afd6e18c95e6efd931a6a3fc4ec0685dff2c6448
glenstewartdev <glenstewartdev@gmail.com>
2024-08-30 18:09:39 -0400

    add models for episode details

--- src/app/services/tv-shows/tv-show.model.ts
+++ src/app/services/tv-shows/tv-show.model.ts
@@ -10,6 +10,15 @@ export interface TvShow {
   image_thumbnail_path: string;
 }
 
+export type TvShowGenre = "Drama" | "Action" | "Science Fiction";
+
+export interface TvShowEpisode {
+  season: number;
+  episode: number;
+  name: string;
+  air_date: string;
+}
+
 export interface TvShowPage {
   total: number;
   page: number;
@@ -20,4 +29,28 @@ export interface TvShowPage {
 export interface TvShowPageState {
   totalPages: number;
   currentPage: number;
+}
+
+export interface TvShowDetail {
+  id: number;
+  name: string;
+  permalink: string;
+  url: string;
+  description: string;
+  description_source: string;
+  start_date: string;
+  end_data: string | null;
+  country: string;
+  status: string;
+  runtime: number;
+  network: string;
+  youtube_link: string | null;
+  image_path: string;
+  image_thumbnail_path: string;
+  rating: number;
+  rating_count: number;
+  countdown: string | null; // ?
+  genres: TvShowGenre[];
+  pictures: string[];
+  episodes: TvShowEpisode[];
 }
