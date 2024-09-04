c90c8b82005d32e2ad2bec7599600e1945156e12
glenstewartdev <glenstewartdev@gmail.com>
2024-09-04 10:56:32 -0400

    refactored show details component template

--- src/app/views/tv-show-detail-view/tv-show-detail-view.component.ts
+++ src/app/views/tv-show-detail-view/tv-show-detail-view.component.ts
@@ -1,14 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { TvShowDetail } from 'src/app/services/tv-shows/tv-show.model';
+import {DatePipe, DecimalPipe} from '@angular/common';
 
 @Component({
   selector: 'app-tv-show-detail-view',
   standalone: true,
-  imports: [],
+  imports: [DatePipe, DecimalPipe],
   templateUrl: './tv-show-detail-view.component.html',
   styleUrl: './tv-show-detail-view.component.css'
 })
 export class TvShowDetailViewComponent {
 
   @Input() tvShowDetail!: TvShowDetail;
+
+  public back() {
+    history.back();
+  }
 }
