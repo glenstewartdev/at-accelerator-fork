b91f6c526174f5c067d00c2bdbc57239f0b8a21f
glenstewartdev <glenstewartdev@gmail.com>
2024-09-10 18:19:46 -0400

    new countdown pipe

--- src/app/views/favorites-card/favorites-card.component.ts
+++ src/app/views/favorites-card/favorites-card.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { TvShowDetail } from 'src/app/services/tv-shows/tv-show.model';
+import { CountdownPipe } from "../../pipes/countdown.pipe";
 
 @Component({
   selector: 'app-favorites-card',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, CountdownPipe],
   templateUrl: './favorites-card.component.html',
   styleUrl: './favorites-card.component.css'
 })
